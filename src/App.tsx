52927f0c4b962b3ef198b2b84991810f197e3c35
Marcos Vinícius <mviniciusmarquesvk@hotmail.com>
2022-12-20 19:27:10 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { MainRoutes } from "./routes/MainRoutes"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Galeria de fotos</h1>
+      <hr />
+      <MainRoutes />
+    </div>
+  )
+}
+
+export default App
+
+
+/* 
+  https://jsonplaceholder.typicode.com/albums
+  https://jsonplaceholder.typicode.com/albums/1
+  https://jsonplaceholder.typicode.com/albumns/1/photos
+  https://jsonplaceholder.typicode.com/photos/1
+*/
